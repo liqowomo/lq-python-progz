166bb09e94185e1706f2f55702f32b08dbaf7c4a
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-01-20 16:23:28 +0000

    Added new code but not printing properly - Trying to get printed to stdout

--- pydir/test.py
+++ pydir/test.py
@@ -9,13 +9,19 @@ import subprocess
 # Lets write some simple ass commands
 
 # This is just running a simple command in 1 line
-ip_info = subprocess.run(['curl', 'ipinfo.io'])
+ip_info = subprocess.run(['curl', 'ipinfo.io'], stdout=True)
+
 
 # Same above command now in a loop
 
 for x in range(5):
-    ip_info
+    print("Printing the args: ", ip_info.args)  # printing the args
+    print("Printing the stdout:",ip_info.stdout)  # Prints nothing
+    print("Printingt the returncode:", ip_info.returncode)  # Prints
+
 
 # Dummy to push comment in
 
-# Just added this line after closing and then opening this with codespaces for testing 
+# Just added this line after closing and then opening this with codespaces for testing
+
+# ip_info variable is empty have to make this clear now
