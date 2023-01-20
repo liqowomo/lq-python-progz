c3234dcfaa45703c79feb427e916ae8d2d220e84
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-01-20 14:43:21 +0000

    Added more python lines

--- pydir/test.py
+++ pydir/test.py
@@ -8,4 +8,10 @@ import subprocess
 
 # Lets write some simple ass commands 
 
+# This is just running a simple command in 1 line 
 ip_info = subprocess.run(['curl', 'ipinfo.io'])
+
+# Same above command now in a loop 
+
+for x in range(5):
+    ip_info
