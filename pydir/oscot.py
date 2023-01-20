eafb057c183a32e6852842ba25cf4d862513dea3
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-01-21 02:58:55 +0530

    Sniff/sn1 (#4)
    * New Quote lines add
    
    * More new Lines added
    
    * \u{1F198} - Adding python directory with simple function
    - Made dir
    - Added test.py
    - File objective is to do simple curl ipinfo
    
    * Added more python lines
    
    * Added more python lines
    
    * Added more python lines
    
    * Loop empty ass commits
    
    * \u{1F004}🇦🇽 - Closed everything and the reopened PR
    - First closed everything
    - Then reopened the the pr through the web interface
    - Now working on this
    
    * Added new code but not printing properly
    - Trying to get printed to stdout
    
    * - Still adding shizz
    
    * 🇬🇧 - Testing out new python file now
    - Making another py
    - This one is for testing the subprocess command
    
    * - Move files around
    
    * adding
    
    * adding
    
    * practicing piping commands beginner
    
    * 0️⃣ - Adding another file just for testing
    - Printing variable on screen for further manipulation
    
    * 🇳🇦 - Getting there with system commands
    - Will have to check this differenly
    - Spending too much time on this
    - Just push this and move on to rust
    
    Co-authored-by: liqowomo <123171512+liqowomo@users.noreply.github.com>

---
+++ pydir/oscot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+Tests of ouptut commands to terminal 
+'''
+
+# subprocess package is recommened for
+
+# main package for running system commands
+import subprocess
+
+ip_me = subprocess.Popen(['curl', 'ipinfo.io'],
+                         stdout=subprocess.PIPE)  # stdout puts it out
+ip_me_jq = subprocess.Popen(
+    ['jq'], stdin=ip_me.stdout)  # stdin is what we want
+
+
