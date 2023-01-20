752ae6e0daa8177b0816abe2f88111ea4c950e89
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-01-20 21:00:31 +0000

    practicing piping commands beginner

--- pydir/oscot.py
+++ pydir/oscot.py
@@ -1,7 +1,13 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 '''
 Tests of ouptut commands to terminal 
 '''
 
-# subprocess package is recommened for 
+# subprocess package is recommened for
+
+# main package for running system commands 
+import subprocess
+
+ip_me = subprocess.Popen(['curl', 'ipinfo.io'], stdout=subprocess.PIPE) #stdout puts it out 
+ip_me_jq = subprocess.Popen(['jq'], stdin=ip_me.stdout) # stdin is what we want
