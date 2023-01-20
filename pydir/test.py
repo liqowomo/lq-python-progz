a7006defd7fabda6ae0dde96c2b004da27cbb417
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-01-20 14:50:41 +0000

    Loop empty ass commits

--- pydir/test.py
+++ pydir/test.py
@@ -6,15 +6,14 @@ Testing out nice ass os command execution
 
 import subprocess
 
-# Lets write some simple ass commands 
+# Lets write some simple ass commands
 
-# This is just running a simple command in 1 line 
+# This is just running a simple command in 1 line
 ip_info = subprocess.run(['curl', 'ipinfo.io'])
 
-# Same above command now in a loop 
+# Same above command now in a loop
 
 for x in range(5):
     ip_info
-    
-# Dummy to push commetn also
 
+# Dummy to push commetn also
