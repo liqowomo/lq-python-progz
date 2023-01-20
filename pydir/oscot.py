63b094640f5ff58ef3519d46bcd6191488886f0d
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-01-20 21:03:45 +0000

    0️⃣ - Adding another file just for testing - Printing variable on screen for further manipulation

--- pydir/oscot.py
+++ pydir/oscot.py
@@ -6,8 +6,12 @@ Tests of ouptut commands to terminal
 
 # subprocess package is recommened for
 
-# main package for running system commands 
+# main package for running system commands
 import subprocess
 
-ip_me = subprocess.Popen(['curl', 'ipinfo.io'], stdout=subprocess.PIPE) #stdout puts it out 
-ip_me_jq = subprocess.Popen(['jq'], stdin=ip_me.stdout) # stdin is what we want
+ip_me = subprocess.Popen(['curl', 'ipinfo.io'],
+                         stdout=subprocess.PIPE)  # stdout puts it out
+ip_me_jq = subprocess.Popen(
+    ['jq'], stdin=ip_me.stdout)  # stdin is what we want
+
+
