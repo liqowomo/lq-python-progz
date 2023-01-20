2438a3510eb451e4e3c3ff0f2040153b6af473d8
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-01-20 17:28:37 +0000

    - Move files around

---
+++ gitpu.fish
@@ -0,0 +1,6 @@
+#!/bin/fish 
+echo "This will execute the following commands"
+echo " git commit -a = Add all files and then open a commit window"
+echo " git push = This will push it up to your branch"
+git commit -a
+git push
